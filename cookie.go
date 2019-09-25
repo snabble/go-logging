b29048162fdeae709de946c51aee18abd9ab6eb4
Gregor Weckbecker <gregor.weckbecker@snabble.io>
2019-09-25 11:25:41 +0200

    Remove dependency to lib-compose

---
+++ cookie.go
@@ -0,0 +1,61 @@
+package logging
+
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Sebastian Mancke, tarent GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import (
+	"net/http"
+	"strings"
+)
+
+// taken and adapted from net/http
+func ReadCookieValue(h http.Header, cookieName string) (string, bool) {
+	lines, ok := h["Cookie"]
+	if !ok {
+		return "", false
+	}
+
+	for _, line := range lines {
+		parts := strings.Split(strings.TrimSpace(line), ";")
+		if len(parts) == 1 && parts[0] == "" {
+			continue
+		}
+		for i := 0; i < len(parts); i++ {
+			parts[i] = strings.TrimSpace(parts[i])
+			if len(parts[i]) == 0 {
+				continue
+			}
+			name, val := parts[i], ""
+			if j := strings.Index(name, "="); j >= 0 {
+				name, val = name[:j], name[j+1:]
+			}
+			if cookieName == name {
+				if len(val) > 1 && val[0] == '"' && val[len(val)-1] == '"' {
+					val = val[1 : len(val)-1]
+				}
+				return val, true
+			}
+		}
+	}
+	return "", false
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/snabble/go-logging
 go 1.12
 
 require (
-	github.com/golang/mock v1.3.1 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.3.0
-	github.com/tarent/lib-compose v0.0.0-20170829113806-69430f91d1d6
 )
