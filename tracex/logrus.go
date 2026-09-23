bbaf9ced8261c5cf7a6c2b5962967a4c1be5cbcb
Mark Schewe <mark.schewe@snabble.io>
2026-09-23 16:34:34 +0200

    replace unmaintained otel logrus bridge with official contrib package

--- tracex/logrus.go
+++ tracex/logrus.go
@@ -2,17 +2,17 @@ package tracex
 
 import (
 	"github.com/sirupsen/logrus"
-	"github.com/uptrace/opentelemetry-go-extra/otellogrus"
+	"go.opentelemetry.io/contrib/bridges/otellogrus"
 )
 
 // NewLogrusHook creates a new logrus hook for otel with log levels from info and up.
 // This is used by our go-logging library.
 func NewLogrusHook() logrus.Hook {
-	return otellogrus.NewHook(otellogrus.WithLevels(
+	return otellogrus.NewHook("", otellogrus.WithLevels([]logrus.Level{
 		logrus.PanicLevel,
 		logrus.FatalLevel,
 		logrus.ErrorLevel,
 		logrus.WarnLevel,
 		logrus.InfoLevel,
-	))
+	}))
 }
